76034e4beb5a95a25bbdd661ceeb3ee9b242c6b3
Hoang Bui <hoangxuanbui6572@gmail.com>
2018-03-31 22:41:46 -0400

    update

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ process.env.NODE_ENV = 'development';
 const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 let mainWindow;
-let addWindow;
 
 // Listen for app to be ready
 app.on('ready', function(){
@@ -31,50 +30,12 @@ app.on('ready', function(){
   Menu.setApplicationMenu(mainMenu);
 });
 
-// Handle add item window
-function createAddWindow(){
-  addWindow = new BrowserWindow({
-    width: 300,
-    height:200,
-    title:'Add Shopping List Item'
-  });
-  addWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'addWindow.html'),
-    protocol: 'file:',
-    slashes:true
-  }));
-  // Handle garbage collection
-  addWindow.on('close', function(){
-    addWindow = null;
-  });
-}
-
-// Catch item:add
-ipcMain.on('item:add', function(e, item){
-  mainWindow.webContents.send('item:add', item);
-  addWindow.close(); 
-  // Still have a reference to addWindow in memory. Need to reclaim memory (Grabage collection)
-  //addWindow = null;
-});
-
 // Create menu template
 const mainMenuTemplate =  [
   // Each object is a dropdown
   {
     label: 'Option',
     submenu:[
-      {
-        label:'Add Item',
-        click(){
-          createAddWindow();
-        }
-      },
-      {
-        label:'Clear Items',
-        click(){
-          mainWindow.webContents.send('item:clear');
-        }
-      },
       {
         label: 'Quit',
         accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
@@ -85,11 +46,6 @@ const mainMenuTemplate =  [
     ]
   }
 ];
-
-// If OSX, add empty object to menu
-if(process.platform == 'darwin'){
-  mainMenuTemplate.unshift({});
-}
 
 // Add developer tools option if in dev
 if(process.env.NODE_ENV !== 'production'){
